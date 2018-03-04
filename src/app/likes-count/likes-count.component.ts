e6ef16a7ba35d685689a9f23e8708773d7266f33
sagarsys <sagar_sys@live.com>
2018-03-04 14:04:04 +0400

    - Like count component

---
+++ src/app/likes-count/likes-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-likes-count',
+  templateUrl: './likes-count.component.html',
+  styleUrls: ['./likes-count.component.css']
+})
+export class LikesCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
